14a44521e66e2c2d2df75eeabc735547095872e3
Litsis <44249106+DeanDeo@users.noreply.github.com>
2018-12-18 17:40:20 -0600

    updates

---
+++ routing/apiroutes.js
@@ -0,0 +1,93 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+// var friends = require("../data/friends");
+var mysql = require("mysql");
+
+var connection;
+if (process.env.friends) {
+  connection = mysql.createConnection(process.env.friends);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3307,
+    user: "root",
+    password: "root",
+    database: "friends"
+  });
+}
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    //once successfully connected, you may want to query your database for the info you'll need later!
+  }
+});
+
+{
+  ("name");
+  "Ahmed", "photo";
+  "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores";
+  [5, 1, 4, 4, 5, 1, 2, 5, 4, 1];
+}
+
+function loadProfiles() {
+  // Selects all of the data from the MySQL profiles table
+  connection.query("SELECT * FROM profiles", function(err, res) {
+    if (err) throw err;
+    //a fun trick for converting mysql's returned 'rowPacketData' obj into more usable JSON
+    var data = JSON.stringify(res);
+    data = JSON.parse(data);
+    // loop over your data converting the string of numbers into an array (using split??)
+    friends = data;
+  });
+}
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(tableData);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    if (friends.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    } else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    friends.length = [];
+
+    res.json({ ok: true });
+  });
+};
