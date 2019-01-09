7c22e591578c84d01473560c9cfaf05723034fcd
Litsis <44249106+DeanDeo@users.noreply.github.com>
2019-01-09 15:15:37 -0600

    deploy

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./routing/apiroutes")(app);
+require("./routing/htmlroutes")(app);
 
 // =============================================================================
 // LISTENER
