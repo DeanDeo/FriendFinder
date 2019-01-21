549bfb5974733c7aa714bbab398f8ad30c6738a0
Litsis <44249106+DeanDeo@users.noreply.github.com>
2019-01-20 19:32:02 -0600

    updates

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -11,23 +11,13 @@ module.exports = function(app) {
     res.json(tableData);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
     var friendFound = {
       name: "",
       photo: "",
       friendDifference: 1000
     };
     var friendInput = req.body;
-    var friendName = friendInput.name;
-    var friendPhoto = friendInput.photo;
     var friendScores = friendInput.scores;
     // var totalDifference = 0;
 
